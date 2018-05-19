bb7237392666be0c5bf0141f13ba12b3cb1fef42
bmblby <castiglia.giuliano@googlemail.com>
2018-05-19 18:53:20 +0200

    setup base leaflet project

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import * as d3 from "d3";
+import _ from 'lodash';
+import "leaflet/dist/leaflet.css";
+import "../css/style.css";
+import L from "leaflet";
+
+var mymap = L.map('mapid').setView([50.85, 9.88], 5.5);
+console.log("ready to go on in the tutorial!");
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYm1ibGJ5IiwiYSI6ImNqaDg4cXFhbTAxNW0zM3FkZTFpamx6YXEifQ.-R9m-vKuOfwwG_NujYk7iw'
+}).addTo(mymap);
