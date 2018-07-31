69fee5ca399a6b6195e0451ff3f1aac9ba6b4b88
bmblby <castiglia.giuliano@googlemail.com>
2018-07-31 13:43:52 +0200

    set explicit path to dump file

--- setupdb.sh
+++ setupdb.sh
@@ -3,5 +3,5 @@
 createuser -U nyc_user -P nyc_pwd
 createdb -T template0 nyc_db -O nyc_user
 psql -c 'GRANT ALL PRIVILEGES ON DATABASE nyc_db TO nyc_user;' -d nyc_db
-psql nyc_db < $1
+psql nyc_db < ./data/nyc.dump
 psql -c 'GRANT ALL PRIVILEGES ON TABLE ways, nyc TO nyc_user;' -d nyc_db
